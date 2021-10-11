59badc4c321e689cf4fef8dca67f8f4c967ee4dd
Brice01-hub <91666155+Brice01-hub@users.noreply.github.com>
2021-10-11 07:27:31 +0000

    laZone.sql
    amazing

---
+++ laZone.sql
@@ -0,0 +1,58 @@
+Create table client
+(
+id_client INT(20) NOT NULL AUTO_INCREMENT,
+nom VARCHAR(10) NOT NULL,
+prenom VARCHAR(50) NOT NULL,
+date_de_naissance DATE NOT NULL,
+NCNIB INT(15) NOT NULL,
+tel INT(15) NOT NULL,
+tel_urgence INT(15) NOT NULL,
+PRIMARY KEY (id_client)
+);
+Create table jouer
+(
+    id_jouer INT(20) NOT NULL AUTO_INCREMENT,
+    id_joueur1 INT(20) NOT NULL,
+    id_joueur2 INT(20) NOT NULL,
+    id_console INT(20) NOT NULL,
+    heure_debut TIME NOT NULL,
+    temps_de_jeu TIME NOT NULL,
+    PRIMARY key (id_jouer),
+    foreign key (id_joueur1) references client(id_client),
+    foreign key (id_joueur2) REFERENCES client(id_client)
+    
+);
+Create table louer
+(
+    id_louer INT(20) NOT NULL AUTO_INCREMENT,
+    id_joueur INT(20)NOT NULL,
+    date_location DATE NOT NULL,
+    date_retour DATE NOT NULL,
+    id_location INT(20),
+    PRIMARY key(id_louer),
+     foreign key (id_joueur) references client(id_client),
+    FOREIGN key (id_location) REFERENCES liste_location(id_location)
+);
+CREATE TABLE liste_location(
+    id_location int(20) NOT null ,
+    id_equipement1 INT(20) NOT NULL,
+    PRIMARY KEY(id_location),
+    FOREIGN KEY(id_equipement1) REFERENCES equipement(id)
+    );
+create table equipement
+(
+    id INT(20)  NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(50)NOT NULL,
+    couleur VARCHAR(10)NOT NULL,
+    taille VARCHAR(40)NOT NULL,
+    longueur VARCHAR(40)NOT NULL,
+    id_type INT(20)NOT NULL,
+    PRIMARY KEY(id),
+    foreign key (id_type) references type (id)
+);
+create table type
+(
+    id INT(20)  NOT NULL AUTO_INCREMENT,
+    type varchar(25) NOT NULL,
+     PRIMARY key(id)
+);
